d55c5e148dc4b70e032f04bde37d87bba5208358
葫芦娃 <78081228+lengdayu@users.noreply.github.com>
2023-04-19 18:01:01 +0800

    结构调整

---
+++ src/components/header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header"></div>
+</template>
+<script setup lang="ts"></script>
+<style>
+.header {
+  width: 100%;
+  height: 15rem;
+  background-image: url('@/assets/banner1.jpg');
+  background-position: center center;
+  background-size: 100%;
+  background-color: hsl(318, 90%, 15%);
+  background-repeat: no-repeat;
+  animation-name: topbanner;
+  animation-duration: 5s;
+}
+
+@keyframes topbanner {
+  0% {
+    background-size: 100%;
+  }
+  50% {
+    background-size: 110%;
+  }
+  100% {
+    background-size: 100%;
+  }
+}
+</style>
