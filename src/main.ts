ed2adc022e7904002ff4afe4767002ec7388cc71
葫芦娃 <78081228+lengdayu@users.noreply.github.com>
2023-04-23 18:03:34 +0800

    xicons引入，首页背景调整，主题暂定暗黑护眼

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//ui库
+// 通用字体
+import 'vfonts/Lato.css'
+// 等宽字体
+import 'vfonts/FiraCode.css'
+
 const app = createApp(App)
 
 app.use(router)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,24 @@
 <template>
-  <Header></Header>
+  <div class="container">
+    <Portal></Portal>
+    <div class="body"></div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import Header from '@/components/header.vue'
+import Portal from '@/components/Portal.vue'
+console.log('first')
 </script>
+<style lang="less" scoped>
+.container {
+  width: 100%;
+  background-image: url('@/assets/1.jpg');
+  background-position-y: center right;
+  background-repeat: no-repeat;
+  background-size: auto;
+  background-attachment: fixed;
+  .body {
+    min-height: 689px;
+  }
+}
+</style>
